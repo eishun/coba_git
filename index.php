2d885e895defe7553dd226b35e2160fe1b960aeb
Alfa3-PC\Alfa3 <mario@doxadigital.com>
2018-04-18 10:46:13 +0700

    New version

--- index.php
+++ index.php
@@ -3,3 +3,7 @@ include('index_mario.html');
 include('index_rizky.html');
 include('index_husein.html');
 ?>
+ 
+<style rel="stylesheet" href="mario.css"></style>
+<style rel="stylesheet" href="rizky.css"></style>
+<style rel="stylesheet" href="husein.css"></style>
