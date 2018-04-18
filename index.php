a6d2ae08b2ada4cc72ba12e6a2521ab819a69c21
Alfa3-PC\Alfa3 <mario@doxadigital.com>
2018-04-18 10:50:17 +0700

    New Version - Mario start

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ include('index_rizky.html');
 include('index_husein.html');
 ?>
  
-<style rel="stylesheet" href="mario.css"></style>
-<style rel="stylesheet" href="rizky.css"></style>
-<style rel="stylesheet" href="husein.css"></style>
+<link  rel="stylesheet" href="mario.css">
+<link rel="stylesheet" href="rizky.css">
+<link rel="stylesheet" href="husein.css">
